5adab606eadf64d036df02250e06ad3f5c8a6842
zr1ram <sriram.varadarajulu>
2026-08-27 21:08:55 +0530

    routing (in general) to  about page like this

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About us</h1>
+      <p>This page is rendered from app/about/page.tsx</p>
+    </main>
+  )
+}
+
+export default AboutPage
