51bf011b494a072cf7ea7505b7b27cdf4a70c9c8
zr1ram <sriram.varadarajulu>
2026-08-27 21:11:23 +0530

    nested routes example

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+const DashboardPage = () => {
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      <p>This page is rendered from app/dashboard/page.tsx</p>
+    </main>
+  )
+}
+
+export default DashboardPage;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,10 @@
+const DashboardSettingsPage = () => {
+  return (
+    <main>
+      <h1>Dashboard Settings</h1>
+      <p>This page is rendered from app/dashboard/settings/page.tsx</p>
+    </main>
+  )
+}
+
+export default DashboardSettingsPage;     
