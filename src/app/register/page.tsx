8d404d56b88152809d001da6d731392354f7e2ce
zr1ram <sriram.varadarajulu>
2026-09-09 14:00:42 +0530

    register user page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "@/app/_components/RegisterForm";
+
+export const metadata = {
+  title: "Register | Next.js App",
+  description: "Create a new user account",
+};
+
+export default function RegisterPage() {
+  return <RegisterForm />;
+}
