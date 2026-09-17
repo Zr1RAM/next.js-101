65514a9c029fc6471ba47255ff6ded8a609af454
zr1ram <sriram.varadarajulu>
2026-09-17 10:49:22 +0530

    Router cache (Client-side In-Memory Cache) - Caching deep dive

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // [ROUTER CACHE (Client-side In-Memory Cache)]:
+  // Configures how long visited route segments remain cached in the user's browser memory
   // experimental: {
   //   staleTimes: {
   //     dynamic: 0,
-  //     static: 1800, // 30 minutes (prevent client cache eviction from closing modal)
+  //     static: 1800, // 30 minutes (prevents client Router Cache eviction from closing modal)
   //   },
   // },
   images: {
